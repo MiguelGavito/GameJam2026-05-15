2325e58fc685dc4e73df4adad1a9acd21a2d345e
MiguelGavito <mgavitogzz@gmail.com>
2026-05-16 13:23:00 -0600

    Creacion de Menus y controladores globales
    En la escena Pruebas cree un prefab que utilizare para abrir las opciones , pausar el jeugo etc. tambien hice la pantalla de inciio y la de boot

---
+++ Assets/Scripts/Prefabs/PauseMenu.cs
@@ -0,0 +1,93 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class PauseMenu : MonoBehaviour
+{
+    public GameObject pausePanel;
+    public GameObject optionsPanel;
+    public GameObject confirmExitPanel;
+
+    public Slider volumeSlider;
+
+    private bool isPaused = false;
+
+    private void Start()
+    {
+        pausePanel.SetActive(false);
+
+        float volume = PlayerPrefs.GetFloat("VOLUME", 1f);
+        volumeSlider.value = volume;
+
+        AudioListener.volume = volume;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPaused)
+                ResumeGame();
+            else
+                PauseGame();
+        }
+    }
+
+    public void PauseGame()
+    {
+        pausePanel.SetActive(true);
+
+        Time.timeScale = 0f;
+
+        isPaused = true;
+    }
+
+    public void ResumeGame()
+    {
+        pausePanel.SetActive(false);
+
+        Time.timeScale = 1f;
+
+        isPaused = false;
+    }
+
+    public void OpenOptions()
+    {
+        optionsPanel.SetActive(true);
+    }
+
+    public void CloseOptions()
+    {
+        optionsPanel.SetActive(false);
+    }
+
+    public void OpenExitConfirm()
+    {
+        confirmExitPanel.SetActive(true);
+    }
+
+    public void CloseExitConfirm()
+    {
+        confirmExitPanel.SetActive(false);
+    }
+
+    public void QuitToMenu()
+    {
+        Time.timeScale = 1f;
+
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void ChangeVolume(float value)
+    {
+        AudioListener.volume = value;
+
+        PlayerPrefs.SetFloat("VOLUME", value);
+        PlayerPrefs.Save();
+    }
+
+    public void DeletePlayerData()
+    {
+        PlayerPrefs.DeleteAll();
+    }
+}

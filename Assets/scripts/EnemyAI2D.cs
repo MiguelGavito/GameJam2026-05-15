e5bd6f4e60761f82de89d2589873e3c53941ff87
A01412933epzi <a01412933@tec.mx>
2026-05-15 20:38:33 -0600

    Update EnemyAI2D.cs

--- Assets/scripts/EnemyAI2D.cs
+++ Assets/scripts/EnemyAI2D.cs
@@ -1,6 +1,46 @@
 using UnityEngine;
 
-public class EnemyAI2D
+public class EnemyAI2D : MonoBehaviour
 {
-    
+    public Transform player;
+
+    public float speed = 3f;
+    public float stopDistance = 1.5f;
+
+    private Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        if (player == null)
+            return;
+
+        float distance =
+            Vector2.Distance(transform.position, player.position);
+
+        // SI ESTÁ LEJOS → PERSEGUIR
+        if (distance > stopDistance)
+        {
+            Vector2 direction =
+                (player.position - transform.position).normalized;
+
+            rb.linearVelocity = direction * speed;
+        }
+        else
+        {
+            // DETENERSE CERCA
+            rb.linearVelocity = Vector2.zero;
+
+            Attack();
+        }
+    }
+
+    void Attack()
+    {
+        Debug.Log("Enemy attacking");
+    }
 }

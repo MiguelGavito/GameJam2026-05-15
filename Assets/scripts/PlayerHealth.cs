93f5899e66facbb60cdad7ec1c5bdc8735bf675f
A01412933epzi <a01412933@tec.mx>
2026-05-15 20:48:21 -0600

    Update PlayerHealth.cs

--- Assets/scripts/PlayerHealth.cs
+++ Assets/scripts/PlayerHealth.cs
@@ -1,6 +1,32 @@
 using UnityEngine;
 
-public class PlayerHealth
+public class PlayerHealth : MonoBehaviour
 {
-    
+    public float maxHealth = 100f;
+
+    private float currentHealth;
+
+    void Start()
+    {
+        currentHealth = maxHealth;
+
+        Debug.Log("Player HP: " + currentHealth);
+    }
+
+    public void TakeDamage(float damage)
+    {
+        currentHealth -= damage;
+
+        Debug.Log("Player HP: " + currentHealth);
+
+        if (currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        Debug.Log("PLAYER DIED");
+    }
 }

e41012e54f7c8a36f14181d25755d5ded377285e
A01412933epzi <a01412933@tec.mx>
2026-05-15 20:14:42 -0600

    Update PlayerMovement.sc.cs

--- Assets/scripts/PlayerMovement.sc.cs
+++ Assets/scripts/PlayerMovement.sc.cs
@@ -1,6 +1,31 @@
 using UnityEngine;
 
-public class PlayerMovement.sc
+public class PlayerMovement : MonoBehaviour
 {
-    
+    public float moveSpeed = 5f;
+
+    private Rigidbody2D rb;
+
+    private Vector2 movement;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        // INPUT WASD
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        // Normalizar para evitar velocidad diagonal exagerada
+        movement = movement.normalized;
+    }
+
+    void FixedUpdate()
+    {
+        // MOVER JUGADOR
+        rb.linearVelocity = movement * moveSpeed;
+    }
 }

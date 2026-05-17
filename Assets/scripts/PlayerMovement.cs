8596507308f0338cda2c56ef6e441151f0e049cc
CrisH2O <cristhianhuezo22@gmail.com>
2026-05-17 13:35:37 -0600

    Mas animaciones agregadas y merge

--- Assets/scripts/PlayerMovement.cs
+++ Assets/scripts/PlayerMovement.cs
@@ -8,10 +8,17 @@ public class PlayerMovement : MonoBehaviour
     private Vector2 movement;
 
     public Animator animator;
+    
+    // NUEVO: Referencia al dibujo del jugador
+    private SpriteRenderer spriteRenderer; 
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        
+        // Obtenemos el componente al iniciar el juego
+        spriteRenderer = GetComponent<SpriteRenderer>(); 
+        
         Cursor.visible = true;
     }
 
@@ -22,6 +29,21 @@ public class PlayerMovement : MonoBehaviour
         movement.y = Input.GetAxisRaw("Vertical");
 
         movement = movement.normalized;
+
+        // --- NUEVO: Lógica de volteo (Flip) ---
+        // Si nos movemos a la izquierda (negativo), volteamos el sprite
+        if (movement.x < 0)
+        {
+            spriteRenderer.flipX = false;
+        }
+        // Si nos movemos a la derecha (positivo), lo regresamos a la normalidad
+        else if (movement.x > 0)
+        {
+            spriteRenderer.flipX = true;
+        }
+        // Nota: No ponemos "else" a secas para que si el jugador se detiene (movement.x == 0), 
+        // mantenga la última dirección a la que estaba mirando.
+        // ------------------------------------
 
         // 🎮 ANIMACIÓN
         float moving = movement != Vector2.zero ? 1f : 0f;
